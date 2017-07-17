16461c8a0aefb10c32838d9ca77dbacb04212610
Sarah Bertussi <sbertuss@stevens.edu>
2017-07-17 09:20:15 -0400

    motion detector development

--- motion_detector_octomap.cpp
+++ motion_detector_octomap.cpp
@@ -6,6 +6,10 @@
 #include <octomap/OcTreeBaseImpl.h>
 #include <octomap/OcTreeNode.h>
 #include <octomap/AbstractOccupancyOcTree.h>
+#include <vector>
+#include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/MultiArrayDimension.h>
 
 using namespace std;
 octomap::OcTreeNode* oldNode;
@@ -22,31 +26,116 @@ float y;
 float z;
 int i;
 int g;
-float motionPoints[1000000][5];
-int spots = 100;
+int h;
+int j;
+int offset = 6;
 int points = 0;
+vector<float> motionPoints;
+bool present;
+int p;
 
-void motionList()
+void condenceList();
 {
-    for(g = 0; g <= points; g++)
+    for(i = 1; i <= points; i++)
     {
-       
-    }
-    if(g > points)
-    {
-      for(i = 1; motionPoints[i][1] != 0  && i != spots+1; i++);
-      if(i == spots + 1)
-      {
-         ROS_FATAL_STREAM("No room left to store point.");
-      }
+       for(p = last+1; motionPoints[p] == 0; p+offset)
 
-      motionPoints[i][1] = 1;
-      motionPoints[i][2] = x;
-      motionPoints[i][3] = y;
-      motionPoints[i][4] = z;
-      motionPoints[i][5] = 1;
+       int starter = i*offset+1;
+
+       motionPoints[starter] = 1;
+       motionPoints[starter+1] = motionPoints[p+1];
+       motionPoints[starter+2] = motionPoints[p+2];
+       motionPoints[starter+3] = motionPoints[p+3];
+       motionPoints[starter+4] = motionPoints[p+4];
+       motionPoints[starter+5] = motionPoints[p+5];
+
     }
 }
+
+void publisher()
+{
+    condenceList();
+    std_msgs::Int32MultiArray array;
+    array.data.clear();
+
+}
+
+//add a new point to the list
+void writeSpot()
+{
+    for(p = 1; motionPoints[p] == 1; p+offset);
+    g = p + 1;
+    h = p + 2;
+    j = p + 3;
+ 
+    motionPoints[p] = 1;
+    motionPoints[g] = x;
+    motionPoints[h] = y;
+    motionPoints[j] = z;
+    motionPoints[p+4] = 1;
+    motionPoints[p+5] = 0;
+
+    points++;
+}
+
+//remove a point from the list
+void deleteSpot()
+{
+    motionPoints[p] = 0;
+    motionPoints[g] = 0;
+    motionPoints[h] = 0;
+    motionPoints[j] = 0;
+    motionPoints[p+4] = 0;
+    motionPoints[p+5] = 0;
+
+    points--;
+}
+
+//Determine if point is on list
+bool searchList()
+{
+//add for?
+    for(p = 1; motionPoints[p] == 0 && p <= points*offset; p+offset);
+    g = p + 1;
+    h = p + 2;
+    j = p + 3;
+
+    if(motionPoints[g] == x && motionPoints[h] == y && motionPoints[j] == z)
+    {
+       return true;
+    }
+
+    return false;
+}
+
+//Add/Update a Point to the List
+void addMotionList()
+{
+    present = searchList();
+    
+    if(present == false)
+    {
+       writeSpot();
+    }
+
+    else
+    {
+       motionPoints[p+4] = motionPoints[p+4] + 1;
+    }
+}
+
+//discredit a point on the list
+void subtractMotionList()
+{
+    present = searchList();
+
+    if(present == true)
+    {
+       motionPoints[p+5] = motionPoints[p+5] + 1;
+    }
+}
+
+//Locate Possible Dynamic Points
 void test(const octomap_msgs::Octomap &msg)
 {
     if(intialize == 0)
@@ -76,14 +165,18 @@ void test(const octomap_msgs::Octomap &msg)
 
               if (oldOccupied != newOccupied)
               {
-                  motionList();
+                  addMotionList();
+              }
+
+              else
+              {
+                  subtractMotionList();
               }
            }
        }
-    }
 
-//size_t l;
-//l = octree->calcNumNodes();
+       oldOctree = newOctree;
+    }
 }
 
 int main(int argc, char **argv)
@@ -91,6 +184,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "octomap_test");
     ros::NodeHandle nh;
     ros::Subscriber sub=nh.subscribe("/octomap_full", 1, &test);
+    motionPoints.resize(1000000);
     ros::spin();
     return 0;
 }
