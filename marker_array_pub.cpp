9eb95501f43e473208df25b2a2512a25963259f8
Paul Szenher <88paul98@gmail.com>
2017-07-19 09:33:15 -0400

    Fixed for loop

--- marker_array_pub.cpp
+++ marker_array_pub.cpp
@@ -5,25 +5,32 @@
 #include <octomap/OcTree.h>
 #include <octomap/OcTreeNode.h>
 #include <octomap/OcTreeBaseImpl.h>
-#include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/MultiArrayDimension.h>
 #include <vector>
+
+using namespace std;
 
 ros::Publisher ma_pub;
 
+vector<float> points;
 
-void markerPublisher(const std_msgs::Int32MultiArray::ConstPtr& msg) {
+void markerPublisher(const std_msgs::Float32MultiArray::ConstPtr& msg) {
 	visualization_msgs::Marker marker;
 	visualization_msgs::MarkerArray markerArray;
 
-	int array[100000];
 	int t = 0;
-	for (std::vector<int>::const_iterator it = msg->data.begin(); it != msg->data.end(); ++it){
-		array[t] = *it;
+
+	points.clear();
+	for (std::vector<float>::const_iterator it = msg->data.begin(); it != msg->data.end(); ++it){
+		points.push_back(*it);
 		t++;
 	}
 
+	ROS_INFO_STREAM("Finished for loop one");
 
-	for(int i=0; i<=(sizeof(msg)/sizeof(float)); i=i*6) {
+	for(int i=1; i<=msg->data.size(); i+=6) {
 		marker.header.frame_id = "/map";
 		marker.header.stamp = ros::Time::now();
 		marker.ns = "motion_array";
@@ -37,9 +44,9 @@ void markerPublisher(const std_msgs::Int32MultiArray::ConstPtr& msg) {
     	marker.scale.y = scale;
     	marker.scale.z = scale;
 
-    	marker.pose.position.x = array[i+1];
-    	marker.pose.position.y = array[i+2];
-    	marker.pose.position.z = array[i+3];
+    	marker.pose.position.x = points[i+1];
+    	marker.pose.position.y = points[i+2];
+    	marker.pose.position.z = points[i+3];
 
     	marker.color.r = 0.0f;
     	marker.color.g = 1.0f;
@@ -47,12 +54,15 @@ void markerPublisher(const std_msgs::Int32MultiArray::ConstPtr& msg) {
     	marker.color.a = 1.0;
     	marker.lifetime = ros::Duration();
 
+    	ROS_WARN_STREAM(msg->data.size());
+
 		markerArray.markers.push_back(marker);    	
 	}
+	ROS_INFO_STREAM("Publish");
 	ma_pub.publish(markerArray);
 }
 
-int main( int argc, char** argv ) {
+int main(int argc, char **argv ) {
   	ros::init(argc, argv, "marker_array_pub");
   	ros::NodeHandle n;
   	ros::Subscriber array_sub = n.subscribe("/filtered_octo", 1, markerPublisher);
