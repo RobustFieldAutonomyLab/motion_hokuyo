67084776693aa6fe7817f6a74e7566b8c5c36852
Paul Szenher <88paul98@gmail.com>
2017-07-26 19:32:46 -0400

    Modified second object detector and fixed marker array deletion problem

--- marker_array_pub.cpp
+++ marker_array_pub.cpp
@@ -15,6 +15,23 @@ using namespace std;
 ros::Publisher ma_pub;
 
 vector<float> points;
+int last_size = 0;
+
+void clear_markers(){
+  visualization_msgs::Marker marker;
+  visualization_msgs::MarkerArray markerArray;
+
+  for(int i=1; i<=10000; i++){
+  marker.header.frame_id = "/map";
+  marker.header.stamp = ros::Time::now();
+  marker.ns = "motion_array";
+    marker.id = i;
+    marker.type = 1;
+    marker.action = visualization_msgs::Marker::DELETE;
+  }
+  markerArray.markers.push_back(marker);
+  ma_pub.publish(markerArray);
+}
 
 void markerPublisher(const std_msgs::Float32MultiArray::ConstPtr& msg) {
 	visualization_msgs::Marker marker;
@@ -25,19 +42,25 @@ void markerPublisher(const std_msgs::Float32MultiArray::ConstPtr& msg) {
 		points.push_back(*it);
 	}
 
-	ROS_INFO_STREAM("Finished for loop one");
+  if(last_size == 0){
+    last_size = msg->data.size();
+  }
 
-	//markerArray.action = 3;
-	for(int i=1; i<=msg->data.size(); i+=6) {
+	for(int i=1; i<=last_size; i+=6) {
 		marker.header.frame_id = "/map";
 		marker.header.stamp = ros::Time::now();
 		marker.ns = "motion_array";
     	marker.id = i;
     	marker.type = 1;
-    	marker.action = visualization_msgs::Marker::ADD;
+      if (i > msg->data.size()){
+        marker.action = visualization_msgs::Marker::DELETE;        
+      }
+      else{
+    	  marker.action = visualization_msgs::Marker::ADD;
+      }
 
     	//double scale = octomap::OcTreeBaseImpl->getResolution();
-    	double scale = 0.1;
+    	double scale = 0.3;
     	marker.scale.x = scale;
     	marker.scale.y = scale;
     	marker.scale.z = scale;
@@ -52,12 +75,15 @@ void markerPublisher(const std_msgs::Float32MultiArray::ConstPtr& msg) {
     	marker.color.a = 1.0;
     	marker.lifetime = ros::Duration();
 
-    	ROS_WARN_STREAM(msg->data.size());
+    	//ROS_WARN_STREAM(msg->data.size());
 
 		markerArray.markers.push_back(marker);    	
 	}
-	ROS_INFO_STREAM("Publish");
+	//ROS_INFO_STREAM("Publish");
 	ma_pub.publish(markerArray);
+
+  last_size = msg->data.size();
+
 }
 
 int main(int argc, char **argv ) {
--- object_detector_test.cpp
+++ object_detector_test.cpp
@@ -5,79 +5,139 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/MultiArrayLayout.h>
 #include <std_msgs/MultiArrayDimension.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
 using namespace std;
 
 vector<vector<vector<float> > > objects;
-float bound = 5;
+vector<vector<float> > tempPoints;
+vector<vector<float> > importList;
+ros::Publisher ma_pub;
+float bound = 1;
 
 template <typename T>
     bool IsInBounds(const T& value, const T& low, const T& high) {
     return !(value < low) && (value < high);
 }
 
-/*void pointBounder() {  //DEPRECATED Main code transferred to nearest neighbor function
+void pointBounder() {  //DEPRECATED Main code transferred to nearest neighbor function
 
-	for(int j = 0; j < importList.size(); j++){
-		if ((IsInBounds(importList[j][0], low, high) && (IsInBounds(importList[j][1], low, high) && (IsInBounds(importList[j][2], low, high))))){
+	//vector< vector<float> > tempPoints;
+	for(int j = 0; j < importList.size(); j++){  //Add all points inside the bounding box to tempPoints
+		if ((IsInBounds(importList[0][0], importList[j][0]-bound, importList[j][0]+bound) && (IsInBounds(importList[0][1], importList[j][1]-bound, importList[j][1]+bound) && (IsInBounds(importList[0][2], importList[j][2]-bound, importList[j][2]+bound))))){
 			tempPoints.push_back(importList[j]);
 		}
 	}
-	int i = 0;
+
+	/*int i = 0;
 	for(int i; i < tempPoints.size(); i++){
 		string  s = to_string(tempPoints[i][0]) + "," + to_string(tempPoints[i][1]) + "," + to_string(tempPoints[i][2]);
 		ROS_INFO_STREAM(s);
-	}
-}*/
+	}*/
+	return; //tempPoints;
+}
 
-void nearestNeighbor(vector< vector<float> > importList, int start_point){ //Should be split to allow for more modularity
-	vector< vector<float> > tempPoints;
-	for(int j = 0; j < importList.size(); j++){  //Add all points inside the bounding box to tempPoints
-		if ((IsInBounds(importList[j][0], -bound, bound) && (IsInBounds(importList[j][1], -bound, bound) && (IsInBounds(importList[j][2], -bound, bound))))){
-			tempPoints.push_back(importList[j]);
-		}
-	}
-	if (tempPoints.empty()){ //If no other point are inside the bound, mark point as noise
-		
-	}
+void nearestNeighbor(int start_point){ //Should be split to allow for more modularity
 
-	vector< vector<float> > tempObject; //initialize object to be added to 3d objects vector
-	tempObject.push_back(tempPoints[0]);  //Add first point of tempPoints to tempObject
-	tempPoints.erase (tempPoints.end()-1);
-	while(ros::ok()){ //Object creation while loop
-		float min_dist = (2*bound); //minimum distance initialized as bounding box size
-		int min_location;
+	vector< vector<float> > tempObject; //initialize object to be added to objects vector
+	if(tempPoints.size() > 1){
+		tempObject.push_back(tempPoints[0]);  //Add first point of tempPoints to tempObject
+		tempPoints.erase (tempPoints.begin());
+		int i = 0;
+		ROS_ERROR_STREAM(tempPoints.size());
+		while(ros::ok()){ //Object creation while loop
+			float min_dist = (2*bound); //minimum distance initialized as bounding box size
+			int min_location;
 
-		for (int j = 0; j < tempPoints.size(); j++){ //Loop through all points in the tempPoints vector
-			float distance = sqrt(pow((tempObject[0][0]-tempPoints[j][0]),2)+pow((tempObject[0][1]-tempPoints[j][1]),2)+pow((tempObject[0][2]-tempPoints[j][2]),2)); //Calculate euclidean distance between all points inside of cube segment
-			tempPoints[j][3] = distance; //REMOVE? Add distance value to slot 3 of points vector
-			if (distance < min_dist){
-				min_dist = distance; //REMOVE? deprecated?
-				min_location = j; //set min location to be the current index value of the vector if distance value is smallest
+			for (int j = 0; j < tempPoints.size(); j++){ //Loop through all points in the tempPoints vector
+				float distance = sqrt(pow((tempObject[i][0]-tempPoints[j][0]),2)+pow((tempObject[i][1]-tempPoints[j][1]),2)+pow((tempObject[i][2]-tempPoints[j][2]),2)); //Calculate euclidean distance between all points inside of cube segment
+				//tempPoints[j][3] = distance; //REMOVE? Add distance value to slot 3 of points vector
+				if (distance < min_dist){
+					//min_dist = distance; //REMOVE? deprecated?
+					min_location = j; //set min location to be the current index value of the vector if distance value is smallest
+					min_dist = distance;
+					//ROS_ERROR_STREAM(min_dist);
+				}
 			}
+			if (min_dist == 2*bound){ //if min distance is still initialized value then break because no nearest neighbors within threshold
+				//tempObject.clear();
+				break;
+			}
+			else{
+				ROS_INFO_STREAM(tempPoints.size());
+				tempObject.push_back(tempPoints[min_location]); //Add lowest distance point to tempObject
+				tempPoints.erase(tempPoints.begin()+min_location); //Erase this point from tempPoints
+			}
+			++i;
+			//ROS_WARN_STREAM(tempObject.size());
+			//ROS_ERROR_STREAM(tempPoints.size());
 		}
-		if (min_dist == 2*bound){ //if min distance is still initialized value then break because no nearest neighbors within threshold
-			tempObject.clear();
-			break;
-		}
-		tempObject.insert(tempObject.begin(),tempPoints[min_location]); //Add lowest distance point to tempObject
-		tempPoints.erase(tempPoints.begin()+min_location); //Erase this point from tempPoints
-
 	}
-	ROS_WARN_STREAM(tempObject.size());
-	for(int i = 1; i < tempObject.size(); i++){ //Output all tempObject points to console 
+	else{ //If no points are detected dont add the single point as an object and erase the point
+		//tempObject.push_back(tempPoints[0]);
+		tempPoints.erase(tempPoints.begin());
+		//ROS_ERROR_STREAM("else pushback");
+	}
+	//ROS_WARN_STREAM(tempObject.size());
+	for(int i = 0; i < tempObject.size(); i++){ //Output all tempObject points to console 
 			string  s = to_string(tempObject[i][0]) + "," + to_string(tempObject[i][1]) + "," + to_string(tempObject[i][2]);
-			ROS_WARN_STREAM(s);
-			ros::Duration(1).sleep();
-			ROS_INFO_STREAM("Meme");
+			//ROS_WARN_STREAM(s);
 		}
-	ROS_ERROR_STREAM("After publishing for");
 	objects.push_back(tempObject); //Add temporary object (vector of points) to objects vector (vector of objects)
 }
 
+void markerPublisher() {
+	visualization_msgs::Marker marker;
+	visualization_msgs::MarkerArray markerArray;
+
+	//ROS_INFO_STREAM(objects.size());
+
+	//markerArray.action = 3;
+	for(int i=0; i<objects.size(); i++){
+		//ROS_WARN_STREAM(objects[i].size());
+		for(int j=0; j<objects[i].size(); j++) {
+
+
+
+			marker.header.frame_id = "/map";
+			marker.header.stamp = ros::Time::now();
+			marker.ns = "motion_array";
+    		marker.id = j;
+    		marker.type = 1;
+    		//marker.action = visualization_msgs::Marker::DELETEALL;
+    		marker.action = visualization_msgs::Marker::ADD;
+
+    		//double scale = octomap::OcTreeBaseImpl->getResolution();
+    		double scale = 0.3;
+    		marker.scale.x = scale;
+    		marker.scale.y = scale;
+    		marker.scale.z = scale;
+
+    		marker.pose.position.x = objects[i][j][0];
+    		marker.pose.position.y = objects[i][j][1];
+    		marker.pose.position.z = objects[i][j][2];
+
+    		marker.color.r = 0.0f;
+    		marker.color.g = 0.0f;
+    		marker.color.b = 1.0f;
+    		marker.color.a = 1.0;
+    		marker.lifetime = ros::Duration(0);
+
+    		//ROS_WARN_STREAM(msg->data.size());
+
+			markerArray.markers.push_back(marker);    	
+		}
+	}
+	//ROS_INFO_STREAM("Publish");
+	ma_pub.publish(markerArray);
+
+	//objects.clear();
+	tempPoints.clear();
+	importList.clear();
+}
 
 void arrayCallback(const std_msgs::Float32MultiArray::ConstPtr& msg) {
-	vector< vector<float> > importList;
 	int first = 1;
 	int start_point = 0; //Sets index point 0 as the first point to check in the vector
 	for (std::vector<float>::const_iterator it = msg->data.begin(); it < msg->data.end(); it+=3){
@@ -92,17 +152,23 @@ void arrayCallback(const std_msgs::Float32MultiArray::ConstPtr& msg) {
 			node.push_back(*it);
 			//ROS_ERROR_STREAM(*it);
 		}
-		node.push_back(0); //REMOVE? Adds deprecated extra slot in vector for distance measurement
+		//node.push_back(0); //REMOVE? Adds deprecated extra slot in vector for distance measurement
 		importList.push_back(node);
 	}
 	//ROS_WARN_STREAM("Entering nearestNeighbor func");
-	nearestNeighbor(importList, start_point);
-}
+	//while((tempPoints.size() != 0)&&(ros::ok())){
+		pointBounder();
+		nearestNeighbor(start_point);
+	//}
 
+	markerPublisher();
+	return;
+}
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "object_detector");
     ros::NodeHandle nh;
     ros::Subscriber sub=nh.subscribe("/filtered_octo", 1, &arrayCallback);
+    ma_pub = nh.advertise<visualization_msgs::MarkerArray>("object_marker", 1);
     ros::spin();
 }
